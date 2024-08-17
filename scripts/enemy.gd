0b7230c46cb6a6261efdfa8cfbb5b64c4939edba
Chapix <lou.allerme@gmail.com>
2024-08-17 16:48:20 +0200

    Basic enemy wander

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,8 +5,20 @@ signal destroyed_by_player(size: int)
 @export var size: int = 1
 @export var hp: int = 1
 
+var wander_time: float = 0.0
+var wander_vec: Vector2i = Vector2i(0.0, 0.0)
+
 func _ready():
 	connect("body_entered", _on_enemy_body_entered)
+
+func _process(delta):
+	wander_time -= delta
+	if wander_time < 0.0:
+		wander_time = 5.0
+		var angle = randi() % 360
+		wander_vec.x = cos(angle) * 10
+		wander_vec.y = sin(angle) * 10
+	position += wander_vec * delta
 
 func destroy():
 	queue_free()
