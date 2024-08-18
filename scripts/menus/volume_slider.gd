1ab63db5d8fcd9ec91039f9f3d4bb0165d8cdcf3
Pierre Cuquel <p.cuquel@free.fr>
2024-08-19 01:51:51 +0200

    Added some sounds

--- scripts/menus/volume_slider.gd
+++ scripts/menus/volume_slider.gd
@@ -10,7 +10,7 @@ var slideValue = 0.0
 func _ready():
 	label.text = busName
 	busIndex = AudioServer.get_bus_index(busName)
-	hslider.value_changed.connect(_on_h_slider_value_changed)
+	#hslider.value_changed.connect(_on_h_slider_value_changed)
 	
 	hslider.value = db_to_linear(
 		AudioServer.get_bus_volume_db(busIndex)
